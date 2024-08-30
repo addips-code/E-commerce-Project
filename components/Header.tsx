074397616e9ecbea04d9f746856ae8473f75e9bc
addips-code <addips002@gmail.com>
2024-08-30 01:08:54 +0100

    modify some lines on my code

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,87 +1,92 @@
 "use client";
 
-import { Grid2x2, Heart, LayoutGrid, Search, ShoppingCart, User } from "lucide-react"
+import { Grid2X2, Heart, LayoutGrid, Search, ShoppingCart, User } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
 
-function Header() {
+const Header = () => {
     const router = useRouter();
 
-    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault;
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
         const input = e.currentTarget.input.value;
         router.push(`/search?q=${input}`);
     }
+
   return (
-    <header className="flex flex-col bg-walmart md:flex-row items-center px-10 py-7 space-x-5">
+    <header className="flex flex-col md:flex-row items-center bg-walmart px-10 py-7 space-x-5 ">
         <Link href="/" className="mb-5 md:mb-0">
-            <Image
+            <Image 
                 src="https://i.imgur.com/5V4wehM.png"
-                alt="logo of store"
+                alt="Logo"
                 width={150}
                 height={150}
             />
         </Link>
 
-        <form onSubmit={handleSubmit} className="flex items-center rounded-full w-full bg-white flex-1">
+        <form onSubmit={handleSubmit} className=" flex items-center bg-white rounded-full w-full flex-1 ">
             <input 
                 type="text" 
-                name="input"
-                placeholder="Search Products..."
-                className="flex-1 px-4 rounded-l-full outline-none placeholder:text-xs text-black"
+                name="input" 
+                placeholder="Search Everything..." 
+                className=" flex-1 px-4 rounded-l-full outline-none placeholder:text-sm text-black "
             />
             <button type="submit">
-                <Search
-                    className="rounded-full h-10 px-2 w-10 cursor-pointer bg-yellow-400"
-                />
+                <Search className=" rounded-full h-10 w-10 px-2 bg-yellow-400 cursor-pointer "/>
             </button>
         </form>
 
-        <div className="flex space-x-5">
+        <div className=" flex space-x-5 mt-5 md:mt-0 ">
             <Link
-               href={"/"}
-               className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm" 
+                href={"/"}
+                className=" hidden xl:flex text-white font-bold items-center space-x-2 text-sm "
             >
-               <Grid2x2 size={20}/>
-               <p>Departments</p> 
+                <Grid2X2 size={20}/>
+                <p>Departments</p>
             </Link>
 
             <Link
-               href={"/"}
-               className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm" 
+                href={"/"}
+                className=" hidden xl:flex text-white font-bold items-center space-x-2 text-sm "
             >
-               <LayoutGrid size={20}/>
-               <p>Services</p> 
+                <LayoutGrid size={20}/>
+                <p>Services</p>
             </Link>
 
             <Link
-               href={"/"}
-               className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm" 
+                href={"/"}
+                className=" flex text-white font-bold items-center space-x-2 text-sm "
             >
-               <Heart size={20}/>
-               <p className="text-xs font-extralight">Reorder</p>
-               <p>My Items</p> 
+                <Heart size={20}/>
+                <div>
+                    <p className="text-xs font-extralight">Reorder</p>
+                    <p>My Items</p>
+                </div>
             </Link>
 
             <Link
-               href={"/"}
-               className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm" 
+                href={"/"}
+                className=" flex text-white font-bold items-center space-x-2 text-sm "
             >
-               <User size={20}/>
-               <p className="text-xs font-extralight">Sign In</p>
-               <p>Account</p> 
+                <User size={20}/>
+                <div>
+                    <p className="text-xs font-extralight">Sign In</p>
+                    <p>Account</p>
+                </div>
             </Link>
 
             <Link
-               href={"/basket"}
-               className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm" 
+                href={"/basket"}
+                className=" flex text-white font-bold items-center space-x-2 text-sm "
             >
-               <ShoppingCart size={20}/>
-               <p className="text-xs font-extralight">No items</p>
-               <p>$0.00</p> 
+                <ShoppingCart size={20}/>
+                <div>
+                    <p className="text-xs font-extralight">No Items</p>
+                    <p>$0.00</p>
+                </div>
             </Link>
         </div>
     </header>
